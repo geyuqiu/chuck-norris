4cca44253c85ab1f105eb64db38c91d27a0f5fee
Yuqiu Ge <yuqiu.ge@gmail.com>
2025-03-24 23:45:46 +0100

    update to angular 16

--- src/app/services/authenticated.guard.ts
+++ src/app/services/authenticated.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
+import { Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticatedGuard implements CanActivate {
+export class AuthenticatedGuard  {
   constructor(private authenticationService: AuthenticationService, private router: Router) {
   }
 
